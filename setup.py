25c2dd4f635bb85f35472b3b5b744164ea24844e
Eduard <feft99@gmail.com>
2022-04-03 05:10:17 +0600

    Create setup.py for local package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gender-bot',
+    version='0.1',
+    author='Eduard Lik',
+    author_email='feft99@gmail.com',
+    packages=find_packages(),
+    license='LICENSE',
+    description='An awesome gender bot that helps to collect a lot of information.',
+    long_description=open('README.md').read(),
+)
